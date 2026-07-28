64e1b983a0394ec92578f821850e810e0a098522
ethankb2027 <ethankb2027@gmail.com>
2026-07-28 17:17:37 -0400

    created Prisma notes

---
+++ Tech_Stack_Notes/Prisma/JobPostingProject/auth.ts
@@ -0,0 +1,31 @@
+import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "./app/generated/prisma/client";
+
+const prisma = new PrismaClient();
+export const { auth, handlers, signIn, signOut } = NextAuth({
+  secret: process.env.BETTER_AUTH_SECRET,
+  session: {
+    strategy: "jwt",
+  },
+  providers: [GitHub],
+  adapter: PrismaAdapter(prisma),
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+        token.name = user.name;
+      }
+      return token;
+    },
+
+    async session({ session, token }) {
+      if (session.user) {
+        session.user.id = token.id as string;
+        session.user.name = token.name as string;
+      }
+      return session;
+    },
+  },
+});
